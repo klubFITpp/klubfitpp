fb819f5d2063ef2d3cac716ac8e4dda99394d0f2
Alexandr Stehlík <39223728+starastary@users.noreply.github.com>
2024-02-11 15:39:58 +0100

    Jekyll and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-blue': '#0073bc',
+        'primary-yellow': '#f9b000',
+        'primary-blue-hover': '#0073bc',
+        'primary-yellow-hover': '#c58b00',
+      },
+    },
+  },
+  plugins: [],
+}
+
