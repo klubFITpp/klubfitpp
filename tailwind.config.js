f3e57f1945f6eee6ed12e31ddbd5970c4bfc4312
Alexandr Stehlík <39223728+starastary@users.noreply.github.com>
2024-02-11 18:24:06 +0100

    nav and arrow in header

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       colors: {
         'primary-blue': '#0073bc',
         'primary-yellow': '#f9b000',
-        'primary-blue-hover': '#0073bc',
+        'primary-blue-hover': '#005389',
         'primary-yellow-hover': '#c58b00',
       },
     },
